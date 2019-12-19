9b58773640f8976735021fb04f9e767c93375f6f
Joydeep Biswas <joydeepb@cs.utexas.edu>
2019-12-18 19:20:50 -0600

    Updated minimal start script

--- scripts/startup/run_joystick_driver.bash
+++ scripts/startup/run_joystick_driver.bash
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
- 
+
 USER="amrl_user"
-PROJECT_DIR="/home/$USER/f1tenth_course" 
+PROJECT_DIR="/home/$USER/f1tenth_course"
 # Source environment variables
 source /opt/ros/melodic/setup.bash
 # Adding the paths to required packages to ROS_PACKAGE_PATH
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/$USER/f1tenth_course
-export DISPLAY=:0 
-  
-  
-  
-# Run VESC driver 
-roslaunch vesc vesc_driver_node.launch > /dev/null 2>&1 &
- 
-# Run the joystick driver
-pushd $PROJECT_DIR
-./scripts/joystick_teleop.py > /dev/null 2>&1 &
-popd
+export DISPLAY=:0
 
+
+# Print the commands being executed, and exit if any command fails.
+set -x -e
+
+# Run VESC driver
+roslaunch f1tenth_course vesc_driver_node.launch > /dev/null &
+
+# Run the joystick driver
+$PROJECT_DIR/bin/joystick --idx 1 > /dev/null &
--- scripts/startup/run_laser.bash
+++ scripts/startup/run_laser.bash
@@ -2,7 +2,7 @@
 
 USER="amrl_user"
 PATH_TO_LIDAR_LAUNCH=/home/$USER/f1tenth_course/src/hokuyo/launch/hokuyo_10lx.launch
-  
+
 # Source environment variables
 source /opt/ros/melodic/setup.bash
 # Adding the paths to required packages to ROS_PACKAGE_PATH
@@ -11,4 +11,4 @@ export DISPLAY=:0
 
 
 # Hokuyo Lidar
-roslaunch $PATH_TO_LIDAR_LAUNCH  > /dev/null 2>&1 &
+roslaunch $PATH_TO_LIDAR_LAUNCH  > /dev/null &
