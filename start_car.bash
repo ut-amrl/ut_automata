4ca7d18e05de6f945cdd7c414764fb1e74e37bea
amrl-robot <amrl@utlists.utexas.edu>
2020-01-02 11:19:58 -0600

    Joydeepb: Updated scripts

--- start_car.bash
+++ start_car.bash
@@ -1,27 +1,32 @@
-#!/usr/bin/env bash
-
-
-USER="amrl_user"
-PROJECT_DIR="/home/$USER/f1tenth_course"
-PATH_TO_LIDAR_LAUNCH=/home/$USER/f1tenth_course/src/hokuyo/launch/hokuyo_10lx.launch
+#!/bin/bash
 
 # Source environment variables
 source /opt/ros/melodic/setup.bash
+
 # Adding the paths to required packages to ROS_PACKAGE_PATH
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/$USER/f1tenth_course
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/f1tenth_course
 export DISPLAY=:0 
+
+IPADDR="$(ip addr show wlan0 | grep -Po 'inet \K[\d.]+')"
+echo "wlan0 IP address is $IPADDR"
+if [[ "$IPADDR" == "10.147."* ]]; then
+  echo "Setting ROS_IP to $IPADDR"
+  export ROS_IP=$IPADDR
+  export ROS_MASTER_URI=http://$IPADDR:11311
+fi
 
 # Print the commands being executed, and exit if any command fails.
 set -x -e
 
 # Run VESC driver 
-$PROJECT_DIR/bin/vesc_driver > /dev/null &
+./bin/vesc_driver > /dev/null &
 
 # Run the joystick driver
-$PROJECT_DIR/bin/joystick --idx 1 > /dev/null &
+./bin/joystick --idx 1 > /dev/null &
 
 # Hokuyo Lidar
-roslaunch $PATH_TO_LIDAR_LAUNCH  > /dev/null &
+roslaunch src/hokuyo/launch/hokuyo_10lx.launch  > /dev/null &
 
 # ROS Websocket bridge.
 roslaunch rosbridge_server rosbridge_websocket.launch > /dev/null &
+
