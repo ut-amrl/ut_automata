7ab3b75c350140d164821df573ae0b23edce2543
Tongrui Li <tli1998@utexas.edu>
2020-08-29 12:16:35 -0500

    fixed timing out issue

--- findros.py
+++ findros.py
@@ -22,7 +22,7 @@ def get_machines(username):
         print("Could not get list of CS hosts. Make sure you have added your SSH key to this shell session.")
         exit(0)
 
-async def test_command(username, machine, command):
+async def test_command(username, machine, command, time_wait):
     proc = await asyncio.create_subprocess_exec(
         "ssh", 
         "-oStrictHostKeyChecking=no",
@@ -32,7 +32,7 @@ async def test_command(username, machine, command):
         stderr=asyncio.subprocess.DEVNULL
     )
     try:
-        exit_code = await asyncio.wait_for(proc.wait(), 3)
+        exit_code = await asyncio.wait_for(proc.wait(), time_wait)
         if exit_code == 0:
             return (machine, True)
         return (machine, False)
@@ -40,20 +40,20 @@ async def test_command(username, machine, command):
         proc.terminate()
         return (machine, False)
 
-async def test_machines(*, username, command):
-    futures = [test_command(username, machine, command) for machine in get_machines(username)]
+async def test_machines(*, username, command, time_wait):
+    futures = [test_command(username, machine, command, time_wait) for machine in get_machines(username)]
     print("Checking hosts...", file=sys.stderr)
     for future in asyncio.as_completed(futures):
         yield await future
 
-async def search(*, username, command):
-    async for machine, status in test_machines(username=username, command=command):
+async def search(*, username, command, time_wait):
+    async for machine, status in test_machines(username=username, command=command, time_wait=time_wait):
         if not status:
             continue
         print(machine)
 
-async def connect(*, username, command):
-    async for machine, status in test_machines(username=username, command=command):
+async def connect(*, username, command, time_wait):
+    async for machine, status in test_machines(username=username, command=command, time_wait=time_wait):
         if not status:
             continue
         subprocess.run(["ssh", f"{username}@{machine}.cs.utexas.edu"])
@@ -63,6 +63,7 @@ async def connect(*, username, command):
 if __name__ == "__main__":
     ap = ArgumentParser("findros", description="Automatically connect to an unused lab machine with ROS")
     ap.add_argument("username", help="CS lab machine username")
+    ap.add_argument("-t", "--time_wait", nargs="?", const = 30, type=int, help="ssh waiting time before timing out")
     ap.add_argument("-s", "--search", dest="search", action="store_true", help="List machines but don't connect")
     ap.add_argument("-i", "--in-use", dest="inuse", action="store_true", help="Find machines with ROS that *ARE* in use")
     ns = ap.parse_args(sys.argv[1:])
@@ -75,9 +76,9 @@ if __name__ == "__main__":
     try:
         if ns.search:
             loop = asyncio.get_event_loop()
-            loop.run_until_complete(search(username=ns.username, command=command))
+            loop.run_until_complete(search(username=ns.username, command=command, time_wait=ns.time_wait))
         else:   
             loop = asyncio.get_event_loop()
-            loop.run_until_complete(connect(username=ns.username, command=command))
+            loop.run_until_complete(connect(username=ns.username, command=command, time_wait=ns.time_wait))
     except KeyboardInterrupt:
         pass
