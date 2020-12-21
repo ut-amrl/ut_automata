7f5d7700d0886592804fad24f5e898ba16da0068
Joydeep Biswas <joydeepb@cs.utexas.edu>
2020-12-21 15:49:41 -0600

    Organization

--- src/gui/gui_mainwindow.cc
+++ src/gui/gui_mainwindow.cc
@@ -322,7 +322,7 @@ void Exec(const string& cmd) {
 
 void MainWindow::StartCar() {
   const string path = ros::package::getPath("ut_automata");
-  Exec(path + "/start_car.sh");
+  Exec(path + "/scripts/start_car.sh");
 }
 
 void MainWindow::StartRos() {
